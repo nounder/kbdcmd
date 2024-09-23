b27bf7d205c78e3c769301a3e9e6ff876bdce873
Rafael Gutkowski <nounderline@libred.org>
2024-09-23 13:15:40 +0200

    Add info about launchctl error

--- install-service.sh
+++ install-service.sh
@@ -7,6 +7,9 @@ cp $SERVICE_FILE $DST_DIR/$SERVICE_FILE
 
 chmod 644 $DST_DIR/$SERVICE_FILE
 
+# If you encounter "Load failed: 5: Input/output error", try:
+# launchctl unload $HOME/Library/LaunchAgents/org.libred.kbdcmd.plist
+
 launchctl load $DST_DIR/$SERVICE_FILE
 
 launchctl start org.libred.kbdcmd.plist
